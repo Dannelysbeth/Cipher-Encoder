e4fffaefb0343934364d60175e8a316eae6819c9
danifoj315 <danifoj315@student.polsl.pl>
2023-03-07 00:21:07 +0100

    fixed

--- src/main/java/dannelysbeth/jetbrains_academy/Main.java
+++ src/main/java/dannelysbeth/jetbrains_academy/Main.java
@@ -1,7 +1,20 @@
 package dannelysbeth.jetbrains_academy;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Input string:");
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        int number;
+
+        System.out.println("\nThe result:");
+
+        for (char c : str1.toCharArray()) {
+            number = c;
+            String binaryString = String.format("%7s", Integer.toBinaryString(number)).replace(' ', '0');
+            System.out.println(c + " = " + binaryString);
+        }
     }
 }
