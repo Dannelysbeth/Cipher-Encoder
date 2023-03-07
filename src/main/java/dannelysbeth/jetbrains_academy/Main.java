ee4c29cc0a91303ebda7ee22b46f9b3d6cea14a2
danifoj315 <danifoj315@student.polsl.pl>
2023-03-07 23:27:24 +0100

    added chuck norris code to cipher encoder

--- src/main/java/dannelysbeth/jetbrains_academy/Main.java
+++ src/main/java/dannelysbeth/jetbrains_academy/Main.java
@@ -8,13 +8,42 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine();
         int number;
+        System.out.println();
+        System.out.println("The result:");
 
-        System.out.println("\nThe result:");
-
-        for (char c : str1.toCharArray()) {
+        String binaryString = "";
+        for(char c : str1.toCharArray()) {
             number = c;
-            String binaryString = String.format("%7s", Integer.toBinaryString(number)).replace(' ', '0');
-            System.out.println(c + " = " + binaryString);
+            binaryString = binaryString + String.format("%7s", Integer.toBinaryString(number)).replace(' ', '0');
         }
+
+        String s;
+        char [] charArray = binaryString.toCharArray();
+        if (charArray[0] == '0') {
+            s = "00 0";
+        } else {
+            s = "0 0";
+        }
+        char prev = charArray[0];
+
+        for (int i = 1; i < charArray.length; i++) {
+            if (charArray[i] == prev){
+                s = s + "0";
+            } else {
+                if (charArray[i] == '0') {
+                    s = s + " 00 0";
+                } else {
+                    s = s + " 0 0";
+                }
+            }
+            prev = charArray[i];
+        }
+        System.out.println(s);
+
+
+    }
+}
+
+
     }
 }
