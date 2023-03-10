387a18dec21394f0736a8e211b9c58fda3e92db7
danifoj315 <danifoj315@student.polsl.pl>
2023-03-10 23:30:18 +0100

    added chuck norris code to cipher encoder works fine

--- src/main/java/dannelysbeth/jetbrains_academy/Main.java
+++ src/main/java/dannelysbeth/jetbrains_academy/Main.java
@@ -1,23 +1,98 @@
 package dannelysbeth.jetbrains_academy;
 
 import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Input string:");
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine();
-        int number;
         System.out.println();
         System.out.println("The result:");
+        String encodedMessage = decode(str1);
+        System.out.println(encodedMessage);
 
+
+    }
+    public static String decode (String str1) {
+        int space_num = 0;
+        String addString = "";
+        String zeroOrOne = "";
+        for (char c : str1.toCharArray()) {
+            if (c == ' ') {
+                space_num++;
+                if(space_num % 2 == 0) {
+                    zeroOrOne = "";
+                }
+
+            }
+            else if (space_num % 2 == 0) {
+                zeroOrOne = zeroOrOne + "0";
+            } else {
+                if (zeroOrOne.equals("0")) {
+                    addString = addString + "1";
+                } else {
+                    addString = addString + "0";
+                }
+            }
+        }
+        int number = 0;
+        char symbol;
+        String encodedMessage = "";
+        for (int i = 0; i < addString.length(); i++) {
+            int remainder = i % 7;
+            if (addString.charAt(i)=='1'){
+                switch(remainder) {
+                    case 0 :
+                        number = 64;
+                        break;
+                    case 1:
+                        number = number + 32;
+                        break;
+                    case 2:
+                        number += 16;
+                        break;
+                    case 3:
+                        number += 8;
+                        break;
+                    case 4:
+                        number += 4;
+                        break;
+                    case 5:
+                        number += 2;
+                        break;
+                    case 6:
+                        number++;
+                        symbol = (char) number;
+                        encodedMessage = encodedMessage + symbol;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                if (remainder == 0) {
+                    number = 0;
+                } else if (remainder == 6) {
+                    symbol = (char) number;
+                    encodedMessage = encodedMessage + symbol;
+                }
+            }
+        }
+        return encodedMessage;
+    }
+
+    public static String encode(String str1) {
         String binaryString = "";
+        String str2 = "";
+        String str3 = "";
+        int number;
         for(char c : str1.toCharArray()) {
             number = c;
             binaryString = binaryString + String.format("%7s", Integer.toBinaryString(number)).replace(' ', '0');
         }
 
-        String s;
+        String s = new String();
         char [] charArray = binaryString.toCharArray();
         if (charArray[0] == '0') {
             s = "00 0";
@@ -38,12 +113,6 @@ public class Main {
             }
             prev = charArray[i];
         }
-        System.out.println(s);
-
-
-    }
-}
-
-
+        return s;
     }
 }
